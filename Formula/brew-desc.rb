001e786aa246baa933f90298c7450f28110a7bf5
Peter Aronoff <telemachus@arpinum.org>
2014-01-14 10:25:52 -0500

    Add three new formulas

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.9.11'
+  version '0.9.12'
 
   skip_clean 'bin'
 
