5ad6555a30de53820f93e03b331193fad2e85711
Peter Aronoff <telemachus@arpinum.org>
2014-02-22 10:25:33 -0500

    Add cloudfoundry-cli and openlitespeed

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.9.48'
+  version '0.9.49'
 
   skip_clean 'bin'
 
