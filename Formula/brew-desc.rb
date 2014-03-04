b825bef52a34058b219a72717bc74fe99115bc1c
Peter Aronoff <telemachus@arpinum.org>
2014-03-04 10:49:03 -0500

    Add freeipmi mruby pyenv-virtualenvwrapper

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.9.58'
+  version '0.9.59'
 
   skip_clean 'bin'
 
