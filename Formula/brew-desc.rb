3dcf3ec2c197f72ae2020bede77675ebeb965aea
Peter Aronoff <telemachus@arpinum.org>
2014-03-09 16:42:45 -0400

    Add chisel and selecta

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.9.66'
+  version '0.9.67'
 
   skip_clean 'bin'
 
