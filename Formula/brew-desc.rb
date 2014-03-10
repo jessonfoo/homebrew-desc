8f3016b832cc8fb2521b661502b6e1ee13ebf0bc
Peter Aronoff <telemachus@arpinum.org>
2014-03-10 08:01:55 -0400

    Remove mlkit mlton urweb

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.9.68'
+  version '0.9.69'
 
   skip_clean 'bin'
 
