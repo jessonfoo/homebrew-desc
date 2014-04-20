68086f9b67ed53bea2f74e8d88e380cf547b0568
Peter Aronoff <telemachus@arpinum.org>
2014-04-20 07:01:10 -0400

    Add check script to the repo
    As a contributor, one thing you can do is check whether brew-desc is
    missing new formulas or still includes deleted formulas.
    
    The included script `check.sh` does both. It requires git and awk. It's
    also written for the `dash` shell, but runs fine via `bash check.sh`.
    
    The script clones the homebrew repo and then builds two lists, done.txt and
    homebrew.txt. By diffing the two lists, the script shows what new items
    aren't in brew-desc and what removed items still are.
    
    If you're not interested in helping to develop brew-desc, just ignore the
    file.

---
+++ check.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env dash
+
+# if [ -d homebrew ]; then
+#     rm -rf homebrew
+# fi
+
+awk '/^  "/ { print }' brew-desc.rb | sed 's/^  "//' | sed 's/".*$//' \
+    > done.txt
+
+if [ ! -d homebrew ]; then
+    git clone https://github.com/Homebrew/homebrew.git
+else
+    cd homebrew
+    git pull origin master
+    cd "$OLDPWD"
+fi
+ls homebrew/Library/Formula/ | sed 's/\.rb$//' > homebrew.txt
+
+printf "\n# fishfish is an expected diff.\n\n"
+diff done.txt homebrew.txt
+
+# rm -rf homebrew
