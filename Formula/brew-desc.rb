732ca9c29dae0eb2996dff84cebd6ade565fc5b2
Peter Aronoff <telemachus@arpinum.org>
2014-06-05 14:58:47 -0400

    Many new formulas; a few gone

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.9.121'
+  version '0.9.122'
 
   skip_clean 'bin'
 
