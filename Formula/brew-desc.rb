8c72ca9832f8fe1abc6b70fc407d908840a522a5
Peter Aronoff <telemachus@arpinum.org>
2014-06-07 18:22:20 -0400

    Remove three; add emscripten

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.9.122'
+  version '0.9.123'
 
   skip_clean 'bin'
 
