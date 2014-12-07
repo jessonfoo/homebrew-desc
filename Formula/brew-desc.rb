88264c9366ccc90f8adbec742a1aa67d9ab5dd90
Peter Aronoff <telemachus@arpinum.org>
2014-12-07 13:17:02 -0500

    Testing new upgrade method

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,12 +3,14 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'https://github.com/telemachus/homebrew-desc.git'
-  version '0.9.155'
-
-  skip_clean 'bin'
+  version '0.9.156'
 
   def install
-    bin.install 'brew-desc.rb'
-    (bin+'brew-desc.rb').chmod 0755
+    opoo <<-EOS.undent
+      `brew desc` has moved to a new install and upgrade mechanism. You no
+      longer need to install the formula. Instead once you've tapped this
+      repository, you've got the command. And whenever you run `brew update`,
+      this command will be updated automatically.
+    EOS
   end
 end
